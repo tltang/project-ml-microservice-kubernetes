f741e376ce64a6112bc526648cee781cecfabf70
Lisa Tang <tltang@yahoo.com>
2022-09-03 07:13:01 +0000

    add kubernetes script and screenshot

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="tltang168/proj"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+/usr/local/bin/kubectl run udacity --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+/usr/local/bin/kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+/usr/local/bin/kubectl port-forward udacity 8000:80
